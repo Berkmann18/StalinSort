52ad64266b7f457accf8386df629aade9167f353
Berkmann18 <maxieberkmann@gmail.com>
2018-11-08 12:13:37 +0000

    Update(Python): Added tests and documentation
    Added the documentation, unit tests and added the feature that allows to sort in descending order
    
    BREAKING CHANGE: One more parameter (which is optional)
    
    closes #2

--- Python/stalinsort.py
+++ Python/stalinsort.py
@@ -1,11 +1,41 @@
-def sort(data):
+#!/usr/bin/python3
+
+"""StalinSort in python3"""
+
+def sort(data, order='asc'):
+    """
+    Sort a list of numbers the Stalin way (i.e. removes items that aren't in order).
+    Parameters
+    ----------
+    data : number[]
+        List of numbers
+    Returns
+    -------
+    copy
+        Sorted list
+
+    Raises
+    ------
+    Exception
+        Invalid order
+    """
     copy = data[:]
     length = len(copy)
     i = 1
-    while i < length:
-        if not copy[i] >= copy[i - 1]:
-            del(copy[i])
-            length -= 1
-        else:
-            i += 1
+    if order == 'asc':
+        while i < length:
+            if not copy[i] >= copy[i - 1]:
+                copy.remove(copy[i])
+                length -= 1
+            else:
+                i += 1
+    elif order == 'des':
+        while i < length:
+            if not copy[i] <= copy[i - 1]:
+                copy.remove(copy[i])
+                length -= 1
+            else:
+                i += 1
+    else:
+        raise Exception('Invalid order:', order)
     return copy
--- Python/test.py
+++ Python/test.py
@@ -1,12 +1,67 @@
-#!/bin/python3
+#!/usr/bin/python3
+
 from stalinsort import sort
-print
-sorted = [0, 1, 2, 3, 4, 5]
+import unittest
+
+sorted =[0, 1, 2, 3]
 mixed = [0, 2, 1, 8, 4]
-rev = [4, 3, 2, 1]
+reversed = [3, 2, 1, 0]
+almost_sorted = [0, 1, 4, 3, 2]
+almost_sorted0 = [0, -1, 2, 4, 7]
 same = [0, 0, 0]
 
-print('sorted=', sorted, '=>', sort(sorted))
-print('mixed=', mixed, '=>', sort(mixed))
-print('rev=', rev, '=>', sort(rev))
-print('same=', same, '=>', sort(same))
+class AscTest(unittest.TestCase):
+  def test_sorted(self):
+    self.assertListEqual(sort(sorted), sorted)
+  
+  def test_empty(self):
+    self.assertListEqual(sort([]), [])
+  
+  def test_reversed(self):
+    self.assertListEqual(sort(reversed), [3])
+
+  def test_almost_sorted(self):
+    self.assertListEqual(sort(almost_sorted), [0, 1, 4])
+    self.assertListEqual(sort(almost_sorted0), [0, 2, 4, 7])
+  
+  def test_repeated(self):
+    self.assertListEqual(sort(same), same)
+
+class DesTest(unittest.TestCase):
+  def test_sorted(self):
+    self.assertListEqual(sort(sorted, 'des'), [0])
+  
+  def test_empty(self):
+    self.assertListEqual(sort([], 'des'), [])
+  
+  def test_reversed(self):
+    self.assertListEqual(sort(reversed, 'des'), reversed)
+
+  def test_almost_sorted(self):
+    self.assertListEqual(sort(almost_sorted, 'des'), [0])
+    self.assertListEqual(sort(almost_sorted0, 'des'), [0, -1])
+  
+  def test_repeated(self):
+    self.assertListEqual(sort(same, 'des'), same)
+
+class OrderTest(unittest.TestCase):
+  def test_right(self):
+    self.assertListEqual(sort(same, 'asc'), same)
+    self.assertListEqual(sort(same, 'des'), same)
+  
+  def test_wrong(self):
+    with self.assertRaises(Exception):
+      sort(same, 'desc')
+    with self.assertRaises(Exception):
+      sort(same, 'ascending')
+    with self.assertRaises(Exception):
+      sort(same, 'descending')
+    with self.assertRaises(Exception):
+      sort(same, 'DESC')
+    with self.assertRaises(Exception):
+      sort(same, 'DES')
+    with self.assertRaises(Exception):
+      sort(same, 'ASC')
+
+if __name__ == '__main__':
+  unittest.main()
